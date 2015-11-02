c4476caf92bec82496783a5a907b71dc007015ae
OlgaJP <olga.pavlova@jetbrains.com>
2015-11-02 12:46:23 +0100

    code added for IDEA-115841

--- src/data/EntityUnusedInspection.java
+++ src/data/EntityUnusedInspection.java
@@ -3,11 +3,13 @@ package data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Created by Olga Pavlova on 11/2/2015.
  */
 @Entity
+@Table(name = "TABLE_NAME")
 public class EntityUnusedInspection {
 
 
---
+++ src/testdata/FooStatus1.java
@@ -0,0 +1,9 @@
+package testdata;
+
+/**
+ * Created by Olga Pavlova on 11/2/2015.
+ */
+public enum FooStatus1 {
+    created;
+
+}

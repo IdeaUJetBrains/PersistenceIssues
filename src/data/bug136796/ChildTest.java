b727e6e3d8517413f0330e5abbf1005f7eeb6c92
OlgaJP <olga.pavlova@jetbrains.com>
2015-11-16 11:29:43 +0100

    code added for some issues

--- src/data/bug136796/ChildTest.java
+++ src/data/bug136796/ChildTest.java
@@ -1,20 +1,31 @@
 package data.bug136796;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Created by Olga Pavlova on 11/16/2015.
  */
 @Entity
 @Table(name = "CONSTRAINT_TABLE_USAGE", schema = "INFORMATION_SCHEMA")
-public class ChildTest {
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of={"id"})
+public class ChildTest  implements Serializable{
+    private static final long serialVersionUID = 111;
 
     @Id
-
-    private int id;
-
+    private Integer id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "TABLE_NAME")
     public void setId(int id) {
         this.id = id;
     }
